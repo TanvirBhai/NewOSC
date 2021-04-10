94c9a81a96ceefaeb66d226c6a04082406752b02
Tanvir Mahmud <Tanvir Mahmud@10.67.106.225>
2021-04-10 18:00:56 -0400

    Added steps for quick search

---
+++ src/test/java/steps/QuickSearchBrand.java
@@ -0,0 +1,54 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class QuickSearchBrand {
+
+	
+	@Given("^I am on OsCommerce Homepage$")
+	public void i_am_on_OsCommerce_Homepage() {
+	 
+	    
+	}
+
+	@When("^I enter the \"([^\"]*)\" on the quick search edit box$")
+	public void i_enter_the_on_the_quick_search_edit_box(String arg1) {
+	 
+	    
+	}
+
+	@When("^I click on search icon$")
+	public void i_click_on_search_icon() {
+	 
+	    
+	}
+
+	@When("^I click on buy now button of the first item$")
+	public void i_click_on_buy_now_button_of_the_first_item() {
+	 
+	    
+	}
+
+	@When("^I select the first model from the dropdown$")
+	public void i_select_the_first_model_from_the_dropdown() {
+	 
+	    
+	}
+
+	@When("^I click on the add cart button$")
+	public void i_click_on_the_add_cart_button() { 
+	 
+	    
+	}
+
+	@When("^I see sign in pager appears$")
+	public void i_see_sign_in_pager_appears() {
+	 
+	    
+	}
+
+
+	
+	
+}
